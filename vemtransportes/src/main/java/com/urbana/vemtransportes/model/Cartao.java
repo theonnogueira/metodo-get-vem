626480603ca9ccea96dfb8f03bc371ad94548736
theonnogueira <101525395+theonnogueira@users.noreply.github.com>
2022-07-23 02:40:46 -0300

    Add files via upload

---
+++ vemtransportes/src/main/java/com/urbana/vemtransportes/model/Cartao.java
@@ -0,0 +1,84 @@
+package com.urbana.vemtransportes.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_cartao")
+public class Cartao {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // Torna o atribulo id em uma primery key dentro do banco de												// dados;
+	private Long id;
+	
+	@NotNull // Este campo não poderá estar vazio;
+	private Long numeroCartao;
+	
+	@NotNull
+	@Size(min = 5, max = 100) // Define o tamanho minímo e máximo do atributo nome;
+	private String nome;
+
+	private boolean status = true;
+	
+	@NotNull
+	private String tipoCartao;
+
+//	@ManyToOne
+//	@JsonIgnoreProperties("cartao")
+//	private Usuario usuario;
+
+	public Cartao() {
+
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getNumeroCartao() {
+		return numeroCartao;
+	}
+
+	public void setNumeroCartao(Long numeroCartao) {
+		this.numeroCartao = numeroCartao;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public boolean isStatus() {
+		return status;
+	}
+
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+
+	public String getTipoCartao() {
+		return tipoCartao;
+	}
+
+	public void setTipoCartao(String tipoCartao) {
+		this.tipoCartao = tipoCartao;
+	}
+
+
+
+}
